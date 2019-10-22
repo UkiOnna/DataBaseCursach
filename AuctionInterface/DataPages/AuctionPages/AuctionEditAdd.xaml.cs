68fdb3f034ad447f4a2db42d7e6b9a61315cf2dd
UkiOnna <sikirovtemirlan@mail.ru>
2019-10-22 17:18:50 +0600

    add some

--- AuctionInterface/DataPages/AuctionPages/AuctionEditAdd.xaml.cs
+++ AuctionInterface/DataPages/AuctionPages/AuctionEditAdd.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AuctionInterface.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,14 +23,57 @@ namespace AuctionInterface.DataPages.AuctionPages
     {
         Window _window;
         int _id;
+        List<string> data = new List<string>();
         public AuctionEditAdd(Window window)
         {
             InitializeComponent();
+            
+            _window =window;
         }
 
-        public AuctionEditAdd(string name,string Adress,DateTimeOffset auctionDate,string specify,int id,Window window)
+        public AuctionEditAdd(string name,string adress,DateTimeOffset auctionDate,string specify,int id,Window window)
         {
+            InitializeComponent();
+            create.Visibility = Visibility.Hidden;
+            edit.Visibility = Visibility.Visible;
+            this.name.Text = name;
+            this.adress.Text = adress;
+            this.date.Text = auctionDate.DateTime.ToShortDateString();
+            this.specify.Text = specify;
+            _id = id;
+            _window = window;
+        }
 
+        private void Create(object sender, RoutedEventArgs e)
+        {
+            
+            if (!IsDataNull())
+            {
+                using (var context = new AuctionContext())
+                {
+                    context.Auctions.Add(new Auction() { Name = name.Text, Adress = adress.Text,AuctionDate=DateTimeOffset.Parse(date.Text),Specify=specify.Text });
+                    context.SaveChanges();
+                }
+                MessageBox.Show("Added");
+                _window.Content = new AuctionPage(_window);
+            }
+            else
+            {
+                MessageBox.Show("Заполните все поля");
+            }
+        }
+
+        private bool IsDataNull()
+        {
+            data.AddRange(new List<string> { name.Text, adress.Text,date.Text,specify.Text });
+            foreach(var str in data)
+            {
+                if (string.IsNullOrEmpty(str))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
